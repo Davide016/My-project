33ae65757d2e244702549502bc347c24319ac63c
Davide016 <68756417+Davide016@users.noreply.github.com>
2020-09-23 22:00:16 +0200

    Update ciao.sh

--- Command line updater for Ubuntu and Debian based distro, also Debian and Ubuntu itself/ciao.sh
+++ Command line updater for Ubuntu and Debian based distro, also Debian and Ubuntu itself/ciao.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "-----------------------------------------------------------------------------------------"
-echo "| Welcome to the comand line sitem updater. This script was designed to automate        |"
+echo "| Welcome to the comand line sistem updater. This script was designed to automate        |"
 echo "| the distro command line process based on Debian, for example Ubuntu, Linux Mint       |"
 echo "| Debian itself etc... I hope you like this script to update the system via command line|"
 echo "| Good Luck.                                                                            |"
